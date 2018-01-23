fc503bd3db3be98504e6ee27c4a212b4e35b4259
Eric Van de Kerckhove <blackdragon.be@gmail.com>
2018-01-23 18:20:09 +0100

    Added shader warmup script

---
+++ Optimization/WarmUpShaders.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Warm up all shaders at the start so there's no stutter when using a previously unused shader.
+/// </summary>
+public class WarmUpShaders : MonoBehaviour
+{
+    private void Start()
+    {
+        Shader.WarmupAllShaders();
+    }
+}
